66cdebc6869d11dd9690e24047031d56c3e3bc41
Vésteinn Bjarnason <vesteinnbjarnason@gmail.com>
2021-03-03 10:38:25 +0000

    navigation bar

---
+++ bubblify/src/components/NavLinks/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavLinks = () => (
+    <ul className="nav-links">
+        <li>
+            <a href="#">Bubbles</a>
+        </li>
+        <li>
+            <a href="#">Bundles</a>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#">Cart</a>
+        </li>
+
+
+    </ul>
+
+);
+
+export default NavLinks;
